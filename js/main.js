6c3795fc9b0f855d928aa0fc418074b6881b6d26
LucaOcchio99 <occhionorelli.luca99@gmail.com>
2021-10-26 08:35:33 +0200

    end

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ const boxes = document.querySelector('.boxes');
 //generatore boxes
 for (let i = 1; i <= 100; i++) {
     //inserisci nuovi box
-    boxes.innerHTML += `<div class="box box-${i}">${i}</div>`
+    //boxes.innerHTML += `<div class="box box-${i}">${i}</div>`;
+    if ((i%3 == 0) && (i%5 == 0)) {
+        boxes.innerHTML += `<div class="box box-5-3">FizzBuzz</div>`;
+    } else if (i%3 == 0){
+        boxes.innerHTML += `<div class="box box-3">fizz</div>`;
+    } else if (i%5 == 0) {
+        boxes.innerHTML += `<div class="box box-5">buzz</div>`;
+    } else {
+        boxes.innerHTML += `<div class="box box-0">${i}</div>`;
+    }
 }
    
